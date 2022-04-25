57ddfd4172593ca116e035f2031cc98433d9b29e
Mathis Dröge <mathis.droege@ewe.net>
2022-04-25 16:33:12 +0200

    Fixup pyinstaller build
    When I used pyinstaller to create the .spec files, it already stored the "--onefile" option in the file so passing it again is (1) redundant and (2) leads to issues with Pyinstaller 5

--- package_release.bat
+++ package_release.bat
@@ -2,8 +2,8 @@ if exist dist/ rmdir /S /Q dist
 if exist Release/ rmdir /S /Q Release
 
 python -m pip install sarc pyqt6 pyinstaller
-pyinstaller -F cli.spec
-pyinstaller -F gui.spec
+pyinstaller cli.spec
+pyinstaller gui.spec
 
 mkdir Release
 xcopy /E /I /Y Input Release\Input
